3ef535de98d27b9be8733c518df074a2ba6e952c
Jordon Bedwell <jordon@envygeeks.io>
2016-01-22 17:32:59 -0600

    Add a YAML benchmarker.

---
+++ benchmark/yaml.rb
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------------------------
+# Frozen-string-literal: true
+# Copyright: 2015-2016 Jordon Bedwell - MIT License
+# Encoding: utf-8
+# ----------------------------------------------------------------------------
+
+require "bundler/setup"
+require "safe_yaml/load"
+require "benchmark/ips"
+require "pathutil"
+
+data = "hello: world\nworld: hello"
+Benchmark.ips :quiet => true do |x|
+  x.json! "benchmark.json"
+  x.report("A:Pathutil.parse_yaml") { Pathutil.parse_yaml(data) }
+  x.report("B:SafeYAML.load") { SafeYAML.load(data) }
+  x.compare!
+end
